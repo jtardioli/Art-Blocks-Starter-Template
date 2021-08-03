67610a3661fcf0f5375294434ecf07da6f018623
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-08-02 20:13:48 -0700

    Log final array

--- public/js/pieces/3x+1.js
+++ public/js/pieces/3x+1.js
@@ -1,34 +1,31 @@
 // Start with example number
 let num = 4783133712;
-console.log(num);
 let nums = [];
 
 // Loop through values and create array
 while (num > 1) {
   if (num % 2 === 1) {
     num = num * 3 + 1;
-    nums.push(num)
-    console.log(num);
+    nums.push(num);
   } else {
     num = num / 2;
-    console.log(num);
-    nums.push(num)
+    nums.push(num);
   }
 }
 
 // Reverse array to start with 1
 nums.reverse();
 
+console.log(nums);
+
 function setup() {
   // Grab the smaller of the window sizes and use that as the canvas size.
-  const smallerDimension =
-    windowWidth < windowHeight ? windowWidth : windowHeight;
-  createCanvas(smallerDimension, smallerDimension);
+  createCanvas(500, 500);
 
-  let x1 = 
-  let y1 = 
-  let x2 =
-  let y2 = 
+  // let x1 =
+  // let y1 =
+  // let x2 =
+  // let y2 =
 
   strokeWeight(1);
   line(30, 20, 85, 75);
